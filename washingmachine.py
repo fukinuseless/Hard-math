9564a1e4659a047ddc434d13d9a88bb1c9cf6dd8
fukinuseless <73313135+fukinuseless@users.noreply.github.com>
2020-10-22 21:27:31 +0100

    Update washingmachine.py

--- washingmachine.py
+++ washingmachine.py
@@ -1,6 +1,18 @@
 pi=3.14159265359
-radius=float(input("What is the radius of your circle: "))
-radiussquared=radius*radius
-ans=radiussquared*pi
-print("The area is",ans)
+ethan_sucks_wang=input("Do you have the radius or diameter of your circle ")
+if ethan_sucks_wang == "radius":
+  radius=float(input("What is the radius of your circle: "))
+  radiussquared=radius*radius
+  ans=radiussquared*pi
+  print("The area is",ans)
+  print("Ethan sucks bare cock")
+
+elif ethan_sucks_wang == "diameter":
+  diameter=float(input("What is the diameter of your circle: "))
+  radius2=diameter/2
+  radius2squared=radius2*radius2
+  ans2=radius2squared*pi
+  print("The area is",ans2)
+  print("Ethan sucks bare cock")
+
 
